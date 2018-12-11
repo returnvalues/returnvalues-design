25ae47442ea7e78e716366ef19713df7ecd9552e
이병준 <byungjun.lee@returnvalues.com>
2018-12-11 19:09:06 +0900

    RdInput 컴포넌트 데이터 양방향 바인딩

--- src/RdInput.vue
+++ src/RdInput.vue
@@ -6,14 +6,20 @@
     <label :class="{'form-check-label':check}">
       <input
         v-if="check"
+        ref="input"
         :type="type"
+        :value="modelValue"
         class="form-check-input"
+        @change="$emit('change',$refs.input.checked)"
       >
       <slot />
       <input
         v-if="!check"
+        ref="input"
+        :value="modelValue"
         :type="type"
         class="form-control"
+        @change="$emit('change',$refs.input.value)"
       >
     </label>
     <slot name="help" />
@@ -23,7 +29,15 @@
 <script>
 export default {
   name: 'RdInput',
-  props: { type: { type: String, default: 'div' } },
+  model: {
+    prop: 'modelValue',
+    event: 'change'
+  },
+
+  props: {
+    type: { type: String, default: 'div' },
+    modelValue: { type: null, default: undefined }
+  },
   computed: {
     check() {
       return this.type === 'checkbox';
