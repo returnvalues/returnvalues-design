98849146f5d369bf9d41ca51c9f7827d7a7551ca
이병준 <byungjun.lee@returnvalues.com>
2018-12-11 18:02:48 +0900

    RdDropdown 컴포넌트 기능 추가

--- pages/index/inputs/Dropdown.vue
+++ pages/index/inputs/Dropdown.vue
@@ -3,30 +3,407 @@
     dropdown
 
     Single button
-    <rd-dropdown>
-      <rd-button
-        slot="button"
-        secondary
-      >
-        text
-      </rd-button>
-      <a
-        href="javascript:"
-      >
-        Action
-      </a>
-      <a
-        href="javascript:"
-      >
-        Another action
-      </a>
-      <a
+           <rd-dropdown>
+             <rd-button
+               slot="button"
+               secondary
+             >
+               text
+             </rd-button>
+             <a
+               href="javascript:"
+             >
+               Action
+             </a>
+             <a
+               href="javascript:"
+             >
+               Another action
+             </a>
+             <a
 
-        href="javascript:"
-      >
-        Something else here
-      </a>
-    </rd-dropdown>
+               href="javascript:"
+             >
+               Something else here
+             </a>
+           </rd-dropdown>
+           <rd-dropdown>
+             <rd-button
+               slot="button"
+               danger
+             >
+               Action
+             </rd-button>
+             <a href="javascript:">
+               Action
+             </a>
+             <a href="javascript:">
+               Another action
+             </a>
+             <a href="javascript:">
+               Something else here
+             </a>
+             <div class="dropdown-divider" />
+             <a href="javascript:">
+               Separated link
+             </a>
+           </rd-dropdown>
+
+           Split button
+
+           <rd-dropdown split>
+             <rd-button
+               slot="button"
+               danger
+             >
+               Action
+             </rd-button>
+             <rd-button
+               slot="button"
+               danger
+             >
+               <span class="sr-only">
+                 Toggle Dropdown
+               </span>
+             </rd-button>
+             <a href="javascript:">
+               Action
+             </a>
+             <a href="javascript:">
+               Another action
+             </a>
+             <a href="javascript:">
+               Something else here
+             </a>
+             <div class="dropdown-divider" />
+             <a href="javascript:">
+               Separated link
+             </a>
+           </rd-dropdown>
+
+           Sizing
+           <rd-dropdown split>
+             <rd-button
+               slot="button"
+               secondary
+               lg
+             >
+               Large button
+             </rd-button>
+
+             <a href="javascript:">
+               Action
+             </a>
+             <a href="javascript:">
+               Another action
+             </a>
+             <a href="javascript:">
+               Something else here
+             </a>
+             <div class="dropdown-divider" />
+             <a href="javascript:">
+               Separated link
+             </a>
+           </rd-dropdown>
+           <rd-dropdown split>
+             <rd-button
+               slot="button"
+               secondary
+               lg
+             >
+               Large split button
+             </rd-button>
+             <rd-button
+               slot="button"
+               secondary
+               lg
+             >
+               <span class="sr-only">
+                 Toggle Dropdown
+               </span>
+             </rd-button>
+
+             <a href="javascript:">
+               Action
+             </a>
+             <a href="javascript:">
+               Another action
+             </a>
+             <a href="javascript:">
+               Something else here
+             </a>
+             <div class="dropdown-divider" />
+             <a href="javascript:">
+               Separated link
+             </a>
+           </rd-dropdown>
+
+           <!-- Small button groups (default and split) -->
+           <rd-dropdown>
+             <rd-button
+               slot="button"
+               secondary
+               sm
+             >
+               Small button
+             </rd-button>
+
+             <a href="javascript:">
+               Action
+             </a>
+             <a href="javascript:">
+               Another action
+             </a>
+             <a href="javascript:">
+               Something else here
+             </a>
+             <div class="dropdown-divider" />
+             <a href="javascript:">
+               Separated link
+             </a>
+           </rd-dropdown>
+           <rd-dropdown split>
+             <rd-button
+               slot="button"
+               secondary
+               sm
+             >
+               Small split button
+             </rd-button>
+             <rd-button
+               slot="button"
+               secondary
+               sm
+             >
+               <span class="sr-only">
+                 Toggle Dropdown
+               </span>
+             </rd-button>
+
+             <a href="javascript:">
+               Action
+             </a>
+             <a href="javascript:">
+               Another action
+             </a>
+             <a href="javascript:">
+               Something else here
+             </a>
+             <div class="dropdown-divider" />
+             <a href="javascript:">
+               Separated link
+             </a>
+           </rd-dropdown>
+
+           Dropright
+
+           <rd-dropdown right>
+             <rd-button
+               slot="button"
+               secondary
+             >
+               Dropright
+             </rd-button>
+             <a href="javascript:">
+               Action
+             </a>
+             <a href="javascript:">
+               Another action
+             </a>
+             <a href="javascript:">
+               Something else here
+             </a>
+             <div class="dropdown-divider" />
+             <a href="javascript:">
+               Separated link
+             </a>
+           </rd-dropdown>
+
+           <rd-dropdown
+             split
+             dropright
+           >
+             <rd-button
+               slot="button"
+               secondary
+             >
+               Split dropright
+             </rd-button>
+             <rd-button
+               slot="button"
+               secondary
+             >
+               <span class="sr-only">
+                 Toggle Dropright
+               </span>
+             </rd-button>
+             <a href="javascript:">
+               Action
+             </a>
+             <a href="javascript:">
+               Another action
+             </a>
+             <a href="javascript:">
+               Something else here
+             </a>
+             <div class="dropdown-divider" />
+             <a href="javascript:">
+               Separated link
+             </a>
+           </rd-dropdown>
+
+           Dropleft
+           <rd-dropdown dropleft>
+             <rd-button
+               slot="button"
+               secondary
+             >
+               Dropleft
+             </rd-button>
+             <a href="javascript:">
+               Action
+             </a>
+             <a href="javascript:">
+               Another action
+             </a>
+             <a href="javascript:">
+               Something else here
+             </a>
+             <div class="dropdown-divider" />
+             <a href="javascript:">
+               Separated link
+             </a>
+           </rd-dropdown>
+
+
+           <rd-dropdown split>
+             <rd-dropdown
+               slot="button"
+               dropleft
+               split
+             >
+               <rd-button
+                 slot="button"
+                 secondary
+               >
+                 <span class="sr-only">
+                   Toggle Dropright
+                 </span>
+               </rd-button>
+               <a href="javascript:">
+                 Action
+               </a>
+               <a href="javascript:">
+                 Another action
+               </a>
+               <a href="javascript:">
+                 Something else here
+               </a>
+               <div class="dropdown-divider" />
+               <a href="javascript:">
+                 Separated link
+               </a>
+             </rd-dropdown>
+             <rd-button
+               slot="button"
+               secondary
+             >
+               Split dropleft
+             </rd-button>
+           </rd-dropdown>
+
+           Menu items
+           <rd-dropdown>
+             <rd-button
+               slot="button"
+               secondary
+             >
+               Dropdown
+             </rd-button>
+             <button
+               class="dropdown-item"
+               type="button"
+             >
+               Action
+             </button>
+             <button
+               class="dropdown-item"
+               type="button"
+             >
+               Another action
+             </button>
+             <button
+               class="dropdown-item"
+               type="button"
+             >
+               Something else here
+             </button>
+           </rd-dropdown>
+
+           Menu alignment
+           <rd-dropdown alignright>
+             <rd-button
+               slot="button"
+               secondary
+             >
+               Right-aligned menu
+             </rd-button>
+             <button
+               class="dropdown-item"
+               type="button"
+             >
+               Action
+             </button>
+             <button
+               class="dropdown-item"
+               type="button"
+             >
+               Another action
+             </button>
+             <button
+               class="dropdown-item"
+               type="button"
+             >
+               Something else here
+             </button>
+           </rd-dropdown>
+
+           Menu content
+           Headers
+
+           <rd-dropdown>
+             <rd-button
+               slot="button"
+               secondary
+             >
+               Header menu
+             </rd-button>
+             <h6 slot="header">
+               Dropdown header
+             </h6>
+             <a href="javascript:">
+               Action
+             </a>
+             <a href="javascript:">
+               Another action
+             </a>
+           </rd-dropdown>
+
+           Text
+           <rd-dropdown>
+             <rd-button
+               slot="button"
+               secondary
+             >
+               Text menu
+             </rd-button>
+             <div class="p-4 text-muted">
+               <p>Some example text that's free-flowing within the dropdown menu.</p>
+               <p class="mb-0">
+                 And this is more example text.
+               </p>
+             </div>
+           </rd-dropdown>
   </div>
 </template>
 
--- src/RdDropdown.vue
+++ src/RdDropdown.vue
@@ -7,7 +7,12 @@ export default {
   },
   props: {
     modelValue: Boolean,
-    tag: { type: String, default: 'button' }
+    tag: { type: String, default: 'button' },
+    split: Boolean,
+    dropright: Boolean,
+    dropleft: Boolean,
+    alignright: Boolean,
+
   },
   data() {
     return { show: this.modelValue };
@@ -22,23 +27,45 @@ export default {
   render(createElement) {
     const { button: buttons = [] } = this.$slots;
     const [button] = buttons.slice(-1);
-    if (button) {
+    if (button && this.$slots.default) {
       const { data } = button;
       data.class = data.class || {};
       data.attrs['data-toggle'] = 'dropdown';
       data.class['dropdown-toggle'] = true;
     }
-    this.$slots.default.forEach((node) => {
-      if (node.tag === 'a') {
+    if (this.$slots.default) {
+      this.$slots.default.forEach((node) => {
+        if (node.tag === 'a' || node.tag === 'button') {
+          const { data } = node;
+          data.class = data.class || {};
+          data.class['dropdown-item'] = true;
+        }
+      });
+    }
+    if (this.$slots.header) {
+      this.$slots.header.forEach((node) => {
         const { data } = node;
         data.class = data.class || {};
-        data.class['dropdown-item'] = true;
+        data.class['dropdown-header'] = true;
+      });
+    }
+    return createElement('div', {
+      class: {
+        [this.split ? 'btn-group' : 'dropdown']: true,
+        dropleft: this.dropleft,
+        dropright: this.dropright
       }
-    });
-    return createElement('div', { class: 'dropdown' }, [
+    }, [
       this.$slots.button,
-      createElement('div', { class: { 'dropdown-menu': true, show: this.show } },
-        this.$slots.default)
+      this.$slots.default && createElement('div',
+        {
+          class: {
+            'dropdown-menu': true,
+            show: this.show,
+            'dropdown-menu-right': this.alignright
+          }
+        },
+        [this.$slots.header, this.$slots.default])
     ]);
   }
 };
