6fa82ced813d798659bcaced63affb37f4122ac9
이병준 <byungjun.lee@returnvalues.com>
2018-12-17 16:57:00 +0900

    RdTooltip 구현

---
+++ src/RdTooltip.vue
@@ -0,0 +1,25 @@
+<template>
+  <comment :is="tag">
+    <slot />
+  </comment>
+</template>
+
+<script>
+export default {
+  name: 'RdTooltip',
+  props: {
+    tag: { type: String, default: 'div' },
+    tooltip: { type: null, default: undefined }
+  },
+  mounted() {
+    if (this.$options.name === 'RdTooltip' || this.tooltip === '' || this.tooltip) {
+      window.$(this.$el).tooltip(this.tooltip || undefined);
+    }
+  }
+
+};
+</script>
+
+<style scoped>
+
+</style>
