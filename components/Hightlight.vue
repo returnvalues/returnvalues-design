d40fbe7ddf53d3d7d2af0f625a223aebd0656474
이병준 <byungjun.lee@returnvalues.com>
2018-12-18 11:15:00 +0900

    hightlight 컴포넌트 구현

---
+++ components/Hightlight.vue
@@ -0,0 +1,22 @@
+<template>
+  <pre><code
+ref="code"
+  :class="type"
+  >{{ code }}</code></pre>
+</template>
+<script>
+
+import hljs from 'highlight.js/lib/highlight';
+import html from 'highlight.js/lib/languages/xml';
+import 'highlight.js/styles/github.css';
+
+
+hljs.registerLanguage('html', html);
+export default {
+  name: 'Hightlight',
+  props: { type: { type: String, default: undefined }, code: { type: String, default: undefined } },
+  mounted() {
+    hljs.highlightBlock(this.$refs.code);
+  }
+};
+</script>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,10 +1,30 @@
 <template>
-  <div>index</div>
+  <div>
+    index
+    <hightlight
+      type="html"
+      code="<!DOCTYPE html>
+<title>Title</title>
+
+<style>body {width: 500px;}</style>
+
+<script type=&quot;application/javascript&quot;>
+  function $init() {return true;}
+</script>
+
+<body>
+  <p checked class=&quot;title&quot; id=&quot;title&quot;>Title</p>
+  <!-- here goes the rest of the page -->
+</body>"
+    />
+  </div>
 </template>
 
 <script>
+import Hightlight from '~/components/Hightlight.vue';
 
 export default {
   name: 'Index',
+  components: { Hightlight },
 };
 </script>
