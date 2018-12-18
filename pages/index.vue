65baca9c3617d3627709a06600ee52110efa1dcb
이병준 <byungjun.lee@returnvalues.com>
2018-12-18 13:22:00 +0900

    Semantic 페이지 추가

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
       :navigations="navigations"
     />
     <div class="col content d-flex flex-grow-1 border rounded py-3 pl-5">
-      <nuxt-child />
+      <nuxt-child  class="container-fluid"/>
     </div>
   </div>
 </template>
@@ -25,9 +25,10 @@ export default {
     return {
       navigations: [
         { name: 'Main', to: '/', },
+        { name: 'Semantic', to: '/semantic', },
+        { name: 'Alert', to: '/alert', },
         { name: 'Card', to: '/card', },
         { name: 'List', to: '/list', },
-        { name: 'Alert', to: '/alert', },
         { name: 'Badge', to: '/badge', },
         { name: 'Breadcrumb', to: '/breadcrumb' },
         { name: 'Carousel', to: '/carousel' },
