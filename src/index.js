b27a479391bc3209118db0a68160a0432e1dbfa2
이병준 <byungjun.lee@returnvalues.com>
2018-12-24 16:22:23 +0900

    Close #2 RdAddress 필터 구현

--- src/index.js
+++ src/index.js
@@ -39,7 +39,14 @@ import RdInputAddress from './RdInputAddress.vue';
 
 export default {
   install(Vue) {
-    Vue.filter('RdAddress', value => `${value}aaaaa`);
+    Vue.filter('RdAddress', (value, length = 26) => {
+      const val = `${value || ''}`;
+      const ret = val.startsWith('0x') ? val.slice(2) : val;
+      const len = length < 9 ? 9 : length;
+      if (ret.length <= len) return `0x${ret}`;
+      const sideLength = (len - 5) / 2;
+      return `0x${ret.slice(0, sideLength + 0.5)}...${ret.slice(-sideLength)}`;
+    });
     Vue.component('RdButton', RdButton);
     Vue.component('RdButtonGroup', RdButtonGroup);
     Vue.component('RdButtonRadio', RdButtonRadio);
