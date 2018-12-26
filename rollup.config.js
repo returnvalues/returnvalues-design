eb843dd602ed96494560674960077beada80ca26
이병준 <byungjun.lee@returnvalues.com>
2018-12-26 20:28:52 +0900

    Close #5 빌드 명령 추가

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,9 @@
 import vue from 'rollup-plugin-vue';
+import fs from 'fs';
+import pkg from './package.json';
+
+if (!fs.existsSync('build')) fs.mkdirSync('build');
+fs.writeFileSync('./build/package.json', JSON.stringify(pkg, null, '  '));
 
 export default {
   entry: 'src/index.js',
