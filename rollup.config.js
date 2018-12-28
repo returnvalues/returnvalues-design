66be23fa0c84108a926cd97c33bf5b0244e9c8bc
이병준 <byungjun.lee@returnvalues.com>
2018-12-28 16:57:19 +0900

    rollup build uglify 추가

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,15 @@
 import vue from 'rollup-plugin-vue';
 import fs from 'fs';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
+
 
 if (!fs.existsSync('build')) fs.mkdirSync('build');
 fs.writeFileSync('./build/package.json', JSON.stringify(pkg, null, '  '));
 
 export default {
   entry: 'src/index.js',
-  plugins: [vue()],
+  plugins: [vue(), terser({ sourcemap: false })],
   output: {
     file: 'build/index.js',
     format: 'esm'
