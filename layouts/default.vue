b7b25008d6407a35e7031e1d53fd9a6c5245a620
이병준 <byungjun.lee@returnvalues.com>
2018-12-29 11:35:30 +0900

    반응형 준비 #12

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="app container-fluid d-flex flex-nowrap p-0">
     <side-nav
-      class="sidenav border-right py-3"
+      class="sidenav border-right py-md-3"
     />
-    <div class="content d-flex flex-grow-1 p-4">
+    <div class="content d-flex flex-grow-1 p-md-4">
       <nuxt class="container-fluid" />
     </div>
   </div>
@@ -22,29 +22,50 @@ export default {
 </script>
 
 <style scoped>
-  .sidenav {
-    width: 240px;
-    min-width: 240px;
-    overflow-y: auto;
-    max-height: 100vh;
-    z-index: 10;
-    flex-wrap: nowrap;
-    position: fixed;
-    height: 100%;
-    background: #fff;
-  }
-  .content{margin-left: 240px;}
+
   .container-fluid {height: 100%}
-  .app{ max-width: 1100px;width: 1100px; }
-  .app::before{
-    content:' ';
+
+  .sidenav{
     position: fixed;
-    width: 100%;
-    height: 100%;
-    background: #f5f8fa;
-    margin-left: 240px;
-    z-index: -1;
+    background: white;
   }
+
+  /* Small */
+  @media (min-width: 576px) {
+    .sidenav{
+      width: 100%;
+      z-index: 10;
+    }
+  }
+  /* Medium */
+  @media (min-width: 768px) {
+    .sidenav {
+      display: block;
+      width: 240px;
+      min-width: 240px;
+      overflow-y: auto;
+      max-height: 100vh;
+      z-index: 10;
+      flex-wrap: nowrap;
+      height: 100%;
+      background: #fff;
+    }
+    .content{margin-left: 240px;}
+    .app{ max-width: 1100px;width: 1100px; }
+    .app::before{
+      content:' ';
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      background: #f5f8fa;
+      margin-left: 240px;
+      z-index: -1;
+    }
+   }
+  /* Large */
+  @media (min-width: 992px) {  }
+  /* Extra large  */
+  @media (min-width: 1200px) { }
 </style>
 
 <style>
