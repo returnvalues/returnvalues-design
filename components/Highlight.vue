54467d8032062938d44685f0a57a2fb52e583e28
이병준 <byungjun.lee@returnvalues.com>
2019-01-09 13:23:12 +0900

    코드 카피 버튼 추가 #17

--- components/Highlight.vue
+++ components/Highlight.vue
@@ -1,8 +1,13 @@
 <template>
-  <pre class="highlight"><code
-ref="code"
-                                         class="p-3 border"
-                                         :class="type"
+  <pre class="highlight"><rd-badge
+  pill
+  secondary
+  class="copy m-1"
+  @click.native="copy"
+>Copy</rd-badge><code
+  ref="code"
+  class="p-3 border"
+  :class="type"
   >{{ code }}</code></pre>
 </template>
 <script>
@@ -23,11 +28,24 @@ export default {
   },
   updated() {
     hljs.highlightBlock(this.$refs.code);
-  }
+  },
+  methods: {
+    copy() {
+      const selection = window.getSelection();
+      selection.removeAllRanges();
+      const range = document.createRange();
+      range.selectNode(this.$refs.code);
+      selection.addRange(range);
+      document.execCommand('copy');
+      selection.removeAllRanges();
+    }
+  },
 };
 </script>
 <style>
 .border + pre.highlight .border{
   border-top: 0px !important;
 }
+.highlight{position: relative;}
+.copy{position: absolute;right:0;cursor: pointer;}
 </style>
