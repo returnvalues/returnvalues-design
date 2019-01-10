1c6993aec9354ec43f99bce9c90cd1647ec34a52
이병준 <byungjun.lee@returnvalues.com>
2019-01-10 15:12:35 +0900

    RdEthCyptocurrency 주석 및 코드정리

--- src/RdEthCyptocurrency.vue
+++ src/RdEthCyptocurrency.vue
@@ -32,7 +32,7 @@ import axios from 'axios';
 export default {
   name: 'RdEthCyptocurrency',
   filters: {
-    numberComma: (n) => {
+    numberComma: (n) => { // 세자리 마다 콤마 찍어주는 필터
       const [int, float] = `${n || 0}`.split('.');
       return int.replace(/\B(?=(\d{3})+(?!\d))/g, ',') + (float ? `.${float}` : '');
     }
@@ -40,10 +40,10 @@ export default {
   props: { currency: { type: String, default: 'KRW' } },
   data() {
     return {
-      currencyDisplay: 'USD',
+      currencyDisplay: 'USD', // 현재 화면에 실제 표시중인 화폐
       data: [],
       exchangeRatePromise: undefined,
-      exchangeRates: { USD: 1 },
+      exchangeRates: { USD: 1 }, // 환율 정보
       cryptocurrencies: {
         ETH: '이더리움',
         BTC: '비트코인',
@@ -87,11 +87,11 @@ export default {
   },
   watch: {
     currency() {
-      if (this.exchangeRates[this.currency]) { // 이미 받아온게 있을 경우 먼저 표시.
+      if (this.exchangeRates[this.currency]) { // 이미 받아온 환율 정보가 있을 경우 먼저 표시.
         this.currencyDisplay = this.currency;
         this.filteredData.forEach(x => this.updateCurrency(x));
       }
-      if (this.currency === 'USD') return;
+      if (this.currency === 'USD') return; // 기준은 USD이기 때문에 스킵
       this.refreshExchangeRate(); // 그 후 새로 고침
       this.exchangeRatePromise.then(() => {
         this.filteredData.forEach(x => this.updateCurrency(x));
@@ -102,16 +102,29 @@ export default {
     this.refresh();
   },
   methods: {
-    updateCurrency(res) {
+    refresh() {
+      this.refreshExchangeRate();
+      Object.keys(this.cryptocurrencies).forEach((base, i) => {
+        axios.get(`https://www.cryptonator.com/api/ticker/${base}-usd`).then(({ data }) => {
+          let res = data && data.success && data.ticker;
+          if (res) {
+            this.setClass(res);
+            this.updateCurrency(res);
+          } else res = { error: data.error, base };
+          this.$set(this.data, i, res); // cryptocurrencies 오브젝트에 있는 순서로 배열 채움
+        });
+      });
+    },
+    updateCurrency(res) { // 환율 적용한 금액 표시 새로고침
       const item = res;
       const props = ['price', 'change', 'volume'];
-      props.forEach((p) => { item[`${p}Display`] = item[p]; });
-      this.exchangeRatePromise.then(() => {
+      props.forEach((p) => { item[`${p}Display`] = item[p]; }); // 일단은 그냥 표시
+      this.exchangeRatePromise.then(() => { // 환율 정보를 가져왔을때 계산하여 표시
         const rate = this.exchangeRates[this.currency];
         props.forEach((p) => { item[`${p}Display`] = +item[p] * rate; });
       });
     },
-    setClass(res) {
+    setClass(res) { // 오르고 내림에 따라 클래스 변함
       const item = res;
       if (+item.change > 0) {
         item.change = `+${item.change}`;
@@ -120,7 +133,7 @@ export default {
         item.class = 'text-primary';
       }
     },
-    refreshExchangeRate() {
+    refreshExchangeRate() { // 환율 새로고침
       this.exchangeRatePromise = axios.get(`https://api.exchangeratesapi.io/latest?base=USD&symbols=${this.currency}`).then((r) => {
         if (r.data && r.data.rates) {
           Object.keys(r.data.rates).forEach((key) => {
@@ -131,19 +144,6 @@ export default {
         return r.data;
       });
     },
-    refresh() {
-      this.refreshExchangeRate();
-      Object.keys(this.cryptocurrencies).forEach((base, i) => {
-        axios.get(`https://www.cryptonator.com/api/ticker/${base}-usd`).then(({ data }) => {
-          let res = data && data.success && data.ticker;
-          if (res) {
-            this.setClass(res);
-            this.updateCurrency(res);
-          } else res = { error: data.error, base };
-          this.$set(this.data, i, res);
-        });
-      });
-    }
   }
 };
 </script>
