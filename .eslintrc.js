5116f0b67a0fb55ca8a239295099394bc330bee8
이병준 <byungjun.lee@returnvalues.com>
2019-01-17 15:03:37 +0900

    RdTextarea html 기본속성 props 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'comma-dangle': 0,
     'import/no-unresolved': 'off',
     'import/no-extraneous-dependencies': 'off',
+    'max-len': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vue/component-name-in-template-casing': ['error', 'kebab-case']
   },
