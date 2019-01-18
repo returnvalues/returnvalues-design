802ead8216c71e338e7cd8e2b522adb53600744d
이병준 <byungjun.lee@returnvalues.com>
2019-01-18 18:54:14 +0900

    rollup 빌드 css 플러그인 추가

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
 import vue from 'rollup-plugin-vue';
 import fs from 'fs';
 import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
 import pkg from './package.json';
-
 
 if (!fs.existsSync('build')) fs.mkdirSync('build');
 fs.writeFileSync('./build/package.json', JSON.stringify(pkg, null, '  '));
 
 export default {
   entry: 'src/index.js',
-  plugins: [vue(), terser({ sourcemap: false })],
+  plugins: [vue(), terser({ sourcemap: false }), postcss()],
   output: {
     file: 'build/index.js',
     format: 'esm'
