c8c34c27bf74ea0a4a35adeede80cd24b8083029
이병준 <byungjun.lee@returnvalues.com>
2019-02-06 11:57:55 +0900

    add title and favicon config

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,14 @@ module.exports = {
     },
   },
   head: {
+    title: 'ReturnValues Design',
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: './favicon.ico',
+      }
+    ],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' }
